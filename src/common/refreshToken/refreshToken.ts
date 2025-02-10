051a2ed78483b579e42309636158b1f584fabbda
denis_tatarinov <skillforce@mail.ru>
2025-02-10 17:46:11 +0300

    + hw8

--- src/common/refreshToken/refreshToken.ts
+++ src/common/refreshToken/refreshToken.ts
@@ -1,6 +1,4 @@
 import { Request, Response } from 'express';
-import { APP_CONFIG } from '../../app_config';
-
 const REFRESH_TOKEN_NAME = 'refreshToken';
 
 export const cookieHandler = {
@@ -8,7 +6,6 @@ export const cookieHandler = {
     res.cookie(REFRESH_TOKEN_NAME, token, {
       httpOnly: true,
       secure: true,
-      maxAge: +APP_CONFIG.RT_TIME,
     });
   },
   getRefreshToken: (req: Request): string | null => {
